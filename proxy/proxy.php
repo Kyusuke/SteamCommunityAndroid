299ba24a53817669d2f4efd1f7fd80fbd60662f2
Kyusuke <sub_zero_death@yahoo.co.uk>
2012-01-30 03:10:11 +0000

    First Commit

---
+++ proxy/proxy.php
@@ -0,0 +1,99 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+
+$filename = "key";
+$handle = fopen($filename, "r");
+$key = fread($handle, filesize($filename));
+fclose($handle);
+
+$mode=$_GET["mode"];
+$steamid=$_GET["steamid"];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+
+//TODO: Add no profile exception
+if (is_numeric($steamid) && strlen($steamid) == 17){
+	}
+else{
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=".$key."&vanityurl=".$steamid."&format=json");
+	$vanity = curl_exec($ch);
+	$vanity = json_decode($vanity, true);
+	$steamid = $vanity['response']['steamid'];
+	curl_close($ch);
+	$ch = curl_init();
+	}
+	
+if ($mode == 'profile'){
+	header("Content-type: application/json");
+	curl_setopt($ch, CURLOPT_URL, "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=".$key."&steamids=".$steamid."&format=json");
+	curl_exec($ch);
+	}
+elseif ($mode == 'friends'){
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=".$key."&steamid=".$steamid."&relationship=friend&format=json");
+	$friends = curl_exec($ch);
+	$friends = json_decode($friends, true);
+	$friendcount = count($friends['friendslist']['friends']);
+	$friendpart = ceil($friendcount / 100);
+	for($i=0; $i < $friendpart; $i++){
+		$friendlistcount = $friendcount - (100 * $i);
+		if ($friendlistcount > 100){
+			$count = 100;
+		}
+		else{
+			$count = $friendlistcount;
+		}
+		for($l=0 + (100 * $i); $l < $count + (100 * $i); $l++){
+			$friendstring[$i] = $friendstring[$i].$friends['friendslist']['friends'][$l]['steamid'].",";
+		}
+	}
+	for($i=0; $i < $friendpart; $i++){
+		$fprofiles = curl_init();
+		curl_setopt($fprofiles, CURLOPT_HEADER, 0);
+		curl_setopt($fprofiles, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($fprofiles, CURLOPT_TIMEOUT, 5);
+		curl_setopt($fprofiles, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($fprofiles, CURLOPT_URL, "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=".$key."&steamids=".$friendstring[$i]."&format=json");
+		$fdata[$i] = curl_exec($fprofiles);
+		$fdata[$i] = json_decode($fdata[$i], true);
+		curl_close($fprofiles);
+	}
+	for($i=0; $i < $friendpart; $i++){
+		$fcount = count($fdata[$i]['response']['players']);
+		for($j=0; $j < $fcount; $j++){
+			$fid = $fdata[$i]['response']['players'][$j]['steamid'];
+			for($k=0; $k < $friendcount; $k++){
+				if($fid == $friends['friendslist']['friends'][$k]['steamid'])
+				{
+					$fstep = (int)($j + ($i * 100));
+					$foutput['summary'][$fstep]['steamid'] = $fdata[$i]['response']['players'][$j]['steamid'];
+					$foutput['summary'][$fstep]['friend_since'] = $friends['friendslist']['friends'][$k]['friend_since'];
+					$foutput['summary'][$fstep]['personaname'] = $fdata[$i]['response']['players'][$j]['personaname'];
+					$foutput['summary'][$fstep]['lastlogoff'] = $fdata[$i]['response']['players'][$j]['lastlogoff'];
+					$foutput['summary'][$fstep]['personastate'] = $fdata[$i]['response']['players'][$j]['personastate'];
+					$foutput['summary'][$fstep]['avatar'] = $fdata[$i]['response']['players'][$j]['avatar'];
+					break 1;
+				}
+			}
+		}
+	}
+	$jsoncounting = count($foutput['summary']);
+	$foutput = json_encode($foutput, JSON_PRETTY_PRINT);
+	header("Content-type: application/json");
+	echo $foutput;
+
+	}
+elseif ($mode == 'backpack'){
+	header("Content-type: application/json");
+	curl_setopt($ch, CURLOPT_URL, "http://api.steampowered.com/IEconItems_440/GetPlayerItems/v0001/?key=".$key."&steamid=".$steamid."&format=json");
+	curl_exec($ch);
+	}
+	
+// TODO: Remember to add in the item schema
+curl_close($ch);
+?> 
