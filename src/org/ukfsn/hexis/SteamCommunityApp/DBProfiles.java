df8438b5b0a23031f16169d62d621cf0df349122
Kyusuke <sub_zero_death@yahoo.co.uk>
2012-04-20 02:49:28 +0100

    SQL setup added

---
+++ src/org/ukfsn/hexis/SteamCommunityApp/DBProfiles.java
@@ -0,0 +1,55 @@
+package org.ukfsn.hexis.SteamCommunityApp;
+
+public class DBProfiles {
+	
+	int favID;
+	String favVanityName;
+	int favSteamID;
+	
+	int hisID;
+	String hisVanityName;
+	int hisSteamID;
+	
+	public void setFavouriteProfile(String favVanityName, int favSteamID){
+		this.favVanityName = favVanityName;
+		this.favSteamID = favSteamID;
+	}
+	
+	public void setFavID(int favID){
+		this.favID = favID;
+	}
+	
+	public int getFavID(){
+		return this.favID;
+	}
+	
+	public String getFavVanityName(){
+		return this.favVanityName;
+	}
+	
+	public int getFavSteamID(){
+		return this.favSteamID;
+	}
+	
+	public void setHistoryProfile(String hisVanityName, int hisSteamID){
+		this.hisVanityName = hisVanityName;
+		this.hisSteamID = hisSteamID;
+	}
+	
+	public void setHisID(int hisID){
+		this.hisID = hisID;
+	}
+	
+	public int getHisID(){
+		return this.hisID;
+	}
+	
+	public String getHisVanityName(){
+		return this.hisVanityName;
+	}
+	
+	public int getHisSteamID(){
+		return this.hisSteamID;
+	}
+	
+}
---
+++ src/org/ukfsn/hexis/SteamCommunityApp/SteamDatabase.java
@@ -0,0 +1,79 @@
+package org.ukfsn.hexis.SteamCommunityApp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SteamDatabase extends SQLiteOpenHelper{
+
+	private static final String dbName = "steamDB";
+	private static final int dbVersion = 1;
+	
+	//Table information for storing favourite Steam profiles
+	private static final String favouriteTable = "Favourites";
+	private static final String favID = "FavouritesID";
+	private static final String favVanityName = "FavouritesVanityName";
+	private static final String favSteamID = "FavouritesSteamID";
+	
+	//Table information for storing recently browsed Steam profiles
+	private static final String historyTable = "History";
+	private static final String hisID = "HistoryID";
+	private static final String hisVanityName = "HistoryVanityName";
+	private static final String hisSteamID = "HistorySteamID";
+	
+	//Table information for storing the modified date of the item schema
+	private static final String schemaModDateTable = "schemaModDateTable";
+	private static final String schemaID = "schemaID";
+	private static final String schemaModDate = "schemaModDate";
+	
+	public SteamDatabase(Context context) {
+		super(context, dbName, null, dbVersion);
+	}
+
+	public void onCreate(SQLiteDatabase db){
+		db.execSQL("CREATE TABLE " +favouriteTable+ " (" +favID+ " INTEGER PRIMARY KEY AUTOINCREMENT, " +favVanityName+ " TEXT, " +favSteamID+ " INTEGER)");
+		db.execSQL("CREATE TABLE " +historyTable+ " (" +hisID+ " INTEGER PRIMARY KEY AUTOINCREMENT, " +hisVanityName+ " TEXT, " +hisSteamID+ " INTEGER)");
+		db.execSQL("CREATE TABLE " +schemaModDateTable+ " (" +schemaID+ " INTEGER PRIMARY KEY AUTOINCREMENT, " +schemaModDate+ " INTEGER)");
+		db.execSQL("INSERT into " +schemaModDateTable+ " (" +schemaID+ ") VALUES ('0')");
+	}
+	
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+		db.execSQL("DROP TABLE IF EXISTS " +favouriteTable);
+		db.execSQL("DROP TABLE IF EXISTS " +historyTable);
+		db.execSQL("DROP TABLE IF EXISTS " +schemaModDateTable);
+		onCreate(db);
+	}
+	
+	void addFavProfile(DBProfiles fav){
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues cv = new ContentValues();
+		
+		cv.put(favVanityName, fav.getFavVanityName());
+		cv.put(favSteamID, fav.getFavSteamID());
+		db.insert(favouriteTable, null, cv);
+		db.close();
+	}
+	
+	public void deleteFavProfile(DBProfiles fav){
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.delete(favouriteTable, favSteamID+ " = " + (String.valueOf(fav.getFavID())), null);
+		db.close();
+	}
+	
+	void addHisProfile(DBProfiles his){
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues cv = new ContentValues();
+		
+		cv.put(hisVanityName, his.getFavVanityName());
+		cv.put(hisSteamID, his.getFavSteamID());
+		db.insert(historyTable, null, cv);
+		db.close();
+	}
+	
+	public void deleteHisProfile(DBProfiles his){
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.delete(historyTable, hisSteamID+ " = " + (String.valueOf(his.getHisID())), null);
+		db.close();
+	}
+}
